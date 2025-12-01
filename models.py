f5181c91d10e6e07c939e90e728a973ad191e614
Thomas Cong <tcong13@mit.edu>
2025-11-30 21:40:16 -0500

    feat: binary classification heartbeat

--- models.py
+++ models.py
@@ -2,7 +2,22 @@ import torch
 
 
 
-
+class MLPClassifier(nn.Module):
+    def __init__(self, T):
+        # T is input length
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(T, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLu()
+            nn.Linear(256, 64),
+            nn.Linear(64, 1)
+        )
+    def forward(self, x):
+        return self.net(x)
 
 def get_model(model_type = "mlp"):
     if model_type = "mlp":
